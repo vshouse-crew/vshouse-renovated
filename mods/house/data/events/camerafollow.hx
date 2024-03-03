58883adeb84fa3763cd7185bf738c84a9062770d
Kolziak <53307233+SpiderKamil@users.noreply.github.com>
2024-03-03 18:53:59 +0100

    camerafollow

---
+++ mods/house/data/events/camerafollow.hx
@@ -0,0 +1,20 @@
+import funkin.game.PlayState;
+import funkin.game.StrumLine;
+import funkin.game.NoteGroup;
+
+function onEvent(eventEvent) {
+	if (eventEvent.event.name == "camerafollow")
+		{
+	if (eventEvent.event.params[0] == 0 && eventEvent.event.params[1] == 0)
+		{
+        }
+		else
+			{
+				curCameraTarget = -1;
+				camFollow.x = eventEvent.event.params[0];
+				camFollow.y = eventEvent.event.params[1];
+			}
+		}
+}
+
+
