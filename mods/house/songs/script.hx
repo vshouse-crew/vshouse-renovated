1739e18c68064dc8123141d30c797c9b3a125a5a
Kolziak <53307233+SpiderKamil@users.noreply.github.com>
2024-03-10 12:21:08 +0100

    g

--- mods/house/songs/script.hx
+++ mods/house/songs/script.hx
@@ -7,6 +7,15 @@ var iconn:FlxSprite;
 var funnitext:FunkinText;
 var funnitext2:FunkinText;
 var funnitext3:FunkinText;
+var tween:FlxTween;
+var tween2:FlxTween;
+var tween3:FlxTween;
+var tween4:FlxTween;
+var tween5:FlxTween;
+var tween6:FlxTween;
+var tween7:FlxTween;
+var tween8:FlxTween;
+var tween9:FlxTween;
 var curArtist;
 
 function create() {
@@ -36,8 +45,8 @@ function create() {
 		{
 	funnitext.y = 320;
 		}
-	funnitext.cameras = [camHUD];
 	funnitext.borderSize = 3;
+	funnitext.cameras = [camHUD];
 	add(funnitext);
 
 	funnitext2 = new FunkinText(-1050, 345, 0, curSong, 45, true);
@@ -61,35 +70,35 @@ function create() {
 function onCountdown() {
 	if(curSong != 'fundament')
 		{
-var tween = FlxTween.tween(bgThing, {x: -500}, 1, {
+tween = FlxTween.tween(bgThing, {x: -500}, 1, {
 		ease: FlxEase.cubeOut,
 	});
-var tween2 = FlxTween.tween(iconn, {x: 320}, 1, {
+tween2 = FlxTween.tween(iconn, {x: 320}, 1, {
+		ease: FlxEase.cubeOut,
+	});
+	tween3 = FlxTween.tween(funnitext, {x: 500}, 1, {
 		ease: FlxEase.cubeOut,
 	});
 	
-		
-	var tween3 = FlxTween.tween(funnitext, {x: 50}, 1, {
-		ease: FlxEase.cubeOut,
-	});
 	if(curSong == 'renovation')
 		{
-	var tween4 = FlxTween.tween(funnitext2, {x: 75}, 1, {
+	tween4 = FlxTween.tween(funnitext2, {x: 75}, 1, {
 		ease: FlxEase.cubeOut,
 	});
     }
     else if(curSong == 'multiversus')
     {
-    var tween4 = FlxTween.tween(funnitext2, {x: 50}, 1, {
+    tween4 = FlxTween.tween(funnitext2, {x: 50}, 1, {
     ease: FlxEase.cubeOut,
     });
     }
-	else{
-		var tween4 = FlxTween.tween(funnitext2, {x: 100}, 1, {
+	else
+		{
+		tween4 = FlxTween.tween(funnitext2, {x: 100}, 1, {
 			ease: FlxEase.cubeOut,
 		});
 	}
-	var tween5 = FlxTween.tween(funnitext3, {x: 25}, 1, {
+	tween5 = FlxTween.tween(funnitext3, {x: 25}, 1, {
 		ease: FlxEase.cubeOut,
 	});
 	if(!downscroll)
@@ -97,13 +106,13 @@ var tween2 = FlxTween.tween(iconn, {x: 320}, 1, {
 			tween3.cancel();
 			tween4.cancel();
 			tween5.cancel();
-	var tween3 = FlxTween.tween(funnitext3, {x: 25}, 1, {
+	tween3 = FlxTween.tween(funnitext3, {x: 25}, 1, {
 		ease: FlxEase.cubeOut,
 	});
-	var tween4 = FlxTween.tween(funnitext2, {x: 100}, 1, {
+	tween4 = FlxTween.tween(funnitext2, {x: 100}, 1, {
 		ease: FlxEase.cubeOut,
 	});
-	var tween5 = FlxTween.tween(funnitext, {x: 50}, 1, {
+	tween5 = FlxTween.tween(funnitext, {x: 50}, 1, {
 		ease: FlxEase.cubeOut,
 	});
 		}
@@ -114,19 +123,19 @@ function stepHit(curStep) {
 		{
 if (curStep == 25)
 	{
-		var tween = FlxTween.tween(bgThing, {x: -1050}, 1, {
+		tween = FlxTween.tween(bgThing, {x: -1050}, 1, {
 			ease: FlxEase.cubeOut,
 		});
-		var tween2 = FlxTween.tween(iconn, {x: -750}, 1, {
+		tween2 = FlxTween.tween(iconn, {x: -750}, 1, {
 			ease: FlxEase.cubeOut,
 		});
-		var tween3 = FlxTween.tween(funnitext, {x: -1050}, 1, {
+		tween3 = FlxTween.tween(funnitext, {x: -1050}, 1, {
 			ease: FlxEase.cubeOut,
 		});
-		var tween4 = FlxTween.tween(funnitext2, {x: -1050}, 1, {
+		tween4 = FlxTween.tween(funnitext2, {x: -1050}, 1, {
 			ease: FlxEase.cubeOut,
 		});
-		var tween5 = FlxTween.tween(funnitext3, {x: -1050}, 1, {
+		tween5 = FlxTween.tween(funnitext3, {x: -1050}, 1, {
 			ease: FlxEase.cubeOut,
 		});
 	}
@@ -135,35 +144,35 @@ else
 	{
 		if(curStep == 256)
 			{
-		var tween = FlxTween.tween(bgThing, {x: -500}, 1, {
+		tween = FlxTween.tween(bgThing, {x: -500}, 1, {
 			ease: FlxEase.cubeOut,
 		});
-	var tween2 = FlxTween.tween(iconn, {x: 320}, 1, {
+	tween2 = FlxTween.tween(iconn, {x: 320}, 1, {
 			ease: FlxEase.cubeOut,
 		});
 		
 			
-		var tween3 = FlxTween.tween(funnitext, {x: 50}, 1, {
+		tween3 = FlxTween.tween(funnitext, {x: 50}, 1, {
 			ease: FlxEase.cubeOut,
 		});
 		if(curSong == 'renovation')
 			{
-		var tween4 = FlxTween.tween(funnitext2, {x: 75}, 1, {
+		tween4 = FlxTween.tween(funnitext2, {x: 75}, 1, {
 			ease: FlxEase.cubeOut,
 		});
 		}
 		else if(curSong == 'multiversus')
 		{
-		var tween4 = FlxTween.tween(funnitext2, {x: 50}, 1, {
+		tween4 = FlxTween.tween(funnitext2, {x: 50}, 1, {
 		ease: FlxEase.cubeOut,
 		});
 		}
 		else{
-			var tween4 = FlxTween.tween(funnitext2, {x: 100}, 1, {
+			tween4 = FlxTween.tween(funnitext2, {x: 100}, 1, {
 				ease: FlxEase.cubeOut,
 			});
 		}
-		var tween5 = FlxTween.tween(funnitext3, {x: 25}, 1, {
+		tween5 = FlxTween.tween(funnitext3, {x: 25}, 1, {
 			ease: FlxEase.cubeOut,
 		});
 		if(!downscroll)
@@ -171,32 +180,32 @@ else
 				tween3.cancel();
 				tween4.cancel();
 				tween5.cancel();
-		var tween3 = FlxTween.tween(funnitext3, {x: 25}, 1, {
+		tween3 = FlxTween.tween(funnitext3, {x: 25}, 1, {
 			ease: FlxEase.cubeOut,
 		});
-		var tween4 = FlxTween.tween(funnitext2, {x: 100}, 1, {
+		tween4 = FlxTween.tween(funnitext2, {x: 100}, 1, {
 			ease: FlxEase.cubeOut,
 		});
-		var tween5 = FlxTween.tween(funnitext, {x: 50}, 1, {
+		tween5 = FlxTween.tween(funnitext, {x: 50}, 1, {
 			ease: FlxEase.cubeOut,
 		});
 	}
 	}
 	if(curStep == 256 + 25)
 		{
-			var tween = FlxTween.tween(bgThing, {x: -1050}, 1, {
+			tween = FlxTween.tween(bgThing, {x: -1050}, 1, {
 				ease: FlxEase.cubeOut,
 			});
-			var tween2 = FlxTween.tween(iconn, {x: -750}, 1, {
+			tween2 = FlxTween.tween(iconn, {x: -750}, 1, {
 				ease: FlxEase.cubeOut,
 			});
-			var tween3 = FlxTween.tween(funnitext, {x: -1050}, 1, {
+			tween3 = FlxTween.tween(funnitext, {x: -1050}, 1, {
 				ease: FlxEase.cubeOut,
 			});
-			var tween4 = FlxTween.tween(funnitext2, {x: -1050}, 1, {
+			tween4 = FlxTween.tween(funnitext2, {x: -1050}, 1, {
 				ease: FlxEase.cubeOut,
 			});
-			var tween5 = FlxTween.tween(funnitext3, {x: -1050}, 1, {
+			tween5 = FlxTween.tween(funnitext3, {x: -1050}, 1, {
 				ease: FlxEase.cubeOut,
 			});
 		}
@@ -205,19 +214,19 @@ else
 }
 function beatHit(curBeat) {
 		funnitext.angle = 2;
-		var tween6 = FlxTween.tween(funnitext, {angle: 0}, 0.2, {
+		tween6 = FlxTween.tween(funnitext, {angle: 0}, 0.2, {
 			ease: FlxEase.circOut,
 		});
 		funnitext2.angle = -2;
-		var tween7 = FlxTween.tween(funnitext2, {angle: 0}, 0.2, {
+		tween7 = FlxTween.tween(funnitext2, {angle: 0}, 0.2, {
 			ease: FlxEase.circOut,
 		});
 		funnitext3.angle = -2;
-		var tween8 = FlxTween.tween(funnitext3, {angle: 0}, 0.2, {
+		tween8 = FlxTween.tween(funnitext3, {angle: 0}, 0.2, {
 			ease: FlxEase.circOut,
 		});
 		iconn.angle = 10;
-		var tween9 = FlxTween.tween(iconn, {angle: 0}, 0.2, {
+		tween9 = FlxTween.tween(iconn, {angle: 0}, 0.2, {
 			ease: FlxEase.circOut,
 		});
 }
