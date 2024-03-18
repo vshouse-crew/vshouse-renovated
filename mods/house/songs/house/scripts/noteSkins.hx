becf632758752bbd6c9ec6cc2450c5d928ad3569
Kolziak <53307233+Kolziak@users.noreply.github.com>
2024-03-18 12:13:55 +0100

    house finished (the song)

---
+++ mods/house/songs/house/scripts/noteSkins.hx
@@ -0,0 +1,28 @@
+function create(){
+    boyfriend.icon = 'bf-awesome';
+}
+
+function postCreate(){
+    missesTxt.visible = false;
+    accuracyTxt.visible = false;
+    healthBar.createFilledBar(FlxColor.fromRGB(255, 0, 0), FlxColor.fromRGB(0, 255, 0));
+    health = health + 0.01;
+    trace(boyfriend.icon);
+}
+
+function update(){
+    lockedIn = true;
+}
+
+function onNoteCreation(event) {
+    event.noteSprite = "game/notes/awesome_notes";
+}
+
+function onStrumCreation(event) {
+    event.sprite = "game/notes/awesome_notes";
+}
+
+function onPlayerHit(e)
+    {
+        e.showSplash = false;
+    }
