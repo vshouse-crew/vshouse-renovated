7ef0b5e19af1f9ea7ead0d7346bce2e0cd8aa377
Kolziak <53307233+Kolziak@users.noreply.github.com>
2024-03-18 10:01:13 +0100

    storymode bg's

--- mods/house/data/states/StoryMenuState.hx
+++ mods/house/data/states/StoryMenuState.hx
@@ -4,6 +4,13 @@ import funkin.backend.FunkinText;
 import flixel.text.FlxText;
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.util.FlxAxes;
+
+public static var weekbgpng:FlxSprite;
+var alreadyCalled:Bool;
+var X:FlxAxes = 0x01;
+var Y:FlxAxes = 0x10;
+var XY:FlxAxes = 0x11;
 
 function postCreate() {
 for(diffSprite in difficultySprites)
@@ -12,5 +19,15 @@ for(diffSprite in difficultySprites)
 	}
     leftArrow.visible = false;
 	rightArrow.visible = false;
-	
+    weekBG.destroy();
+}
+
+function update(){
+	if(alreadyCalled) weekbgpng.destroy();
+	weekbgpng = new FlxSprite().loadGraphic(Paths.image('menus/storymenu/bgs/' + weeks[curWeek].id));
+	weekbgpng.y = 56;
+	weekbgpng.height = 400;
+	weekbgpng.screenCenter(X);
+	add(weekbgpng);
+	alreadyCalled = true;
 }
